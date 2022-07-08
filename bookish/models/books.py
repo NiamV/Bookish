7894b744f8ac7c1a04edfc84e08dd4d7c8e85fbd
Niam Vaishnav <niam.vaishnav@softwire.com>
2022-07-08 13:50:03 +0100

    First Commit

---
+++ bookish/models/books.py
@@ -0,0 +1,26 @@
+from bookish.app import db
+
+
+class Books(db.Model):
+    # This sets the name of the table in the database
+    __tablename__ = 'Books'
+
+    # Here we outline what columns we want in our database
+    isbn = db.Column(db.String(), primary_key=True)
+    title = db.Column(db.String())
+    author = db.Column(db.String())
+
+    def __init__(self, isbn, title, author):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+
+    def __repr__(self):
+        return '<isbn {}>'.format(self.isbn)
+
+    def serialize(self):
+        return {
+            'isbn': self.isbn,
+            'title': self.title,
+            'author': self.author
+        }
---
+++ bookish/models/users.py
@@ -0,0 +1,26 @@
+from bookish.app import db
+
+
+class Users(db.Model):
+    # This sets the name of the table in the database
+    __tablename__ = 'Users'
+
+    # Here we outline what columns we want in our database
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String())
+    accessToken = db.Column(db.String())
+
+    def __init__(self, id, name, accessToken):
+        self.id = id
+        self.name = name
+        self.accessToken = accessToken
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'accessToken': self.accessToken
+        }
