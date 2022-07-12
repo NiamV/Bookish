b6feb054cc381783d6d22f39cd9e6253c52504b9
Alfie Smith <Alfie.Smith@softwire.com>
2022-07-12 17:01:55 +0100

    Finished home page

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react"
-import { BookSearch } from "../BookSearch/BookSearch"
-import { BookDisplay } from "../BookDisplay/BookDisplay";
+import { BookSearch } from "./BookSearch/BookSearch"
+import { BookDisplay } from "./BookDisplay/BookDisplay";
+import { BookishHeader } from "../BookishNavbar/BookishNavbar"
 import {
-  HomeDiv,
   HomeTitleTag,
   HomeTitleContainer,
   HomeSubTitleTag,
@@ -15,17 +15,18 @@ export function HomePage(props) {
   const [books, setBooks] = useState([]);
 
   return (
-      <HomeDiv>
-        <HomeTitleContainer>
-          <HomeTitleTag>BOOKISH</HomeTitleTag>
-          <br />
-          <HomeSubTitleContainer>
-            <HomeSubTitleTag>A library management system you can trust.</HomeSubTitleTag>
-          </HomeSubTitleContainer>
-        </HomeTitleContainer>
-        
-        <BookSearch apiService={props.apiService} books={books} setBooks={setBooks} />
-        <BookDisplay apiService={props.apiService} books={books} setBooks={setBooks} />
-      </HomeDiv>
+    <div>
+      <BookishHeader />
+      <HomeTitleContainer>
+        <HomeTitleTag>BOOKISH</HomeTitleTag>
+        <br />
+        <HomeSubTitleContainer>
+          <HomeSubTitleTag>A library management system you can trust.</HomeSubTitleTag>
+        </HomeSubTitleContainer>
+      </HomeTitleContainer>
+
+      <BookSearch apiService={props.apiService} books={books} setBooks={setBooks} />
+      <BookDisplay apiService={props.apiService} books={books} setBooks={setBooks} />
+    </div>
   );
 }
